7a03581341f1f64fefba900057312211c5be6393
AndrosaKrist <androsa029@gmail.com>
2022-05-31 23:03:42 +0700

    add uuid
    Signed-off-by: AndrosaKrist <androsa029@gmail.com>

--- src/models/user.py
+++ src/models/user.py
@@ -1,29 +1,43 @@
-from enum import IntEnum
+from dataclasses import field
+import email
+import uuid as uuid_pkg
+
+
+from enum import Enum
 from pydantic import BaseModel, Field
 from typing import Optional, Union
 
+
+
 class UserModel(BaseModel):
-    user_id: Optional[int] = None
+    user_id: uuid_pkg.UUID = field(
+        default_factory= uuid_pkg.uuid4, nullable = False)
+    # email: 
     nama: Union[str, None] = Field(
         default=None, max_length=32
-        )
+    )    
     gelombang: list[int] = 1
 
 
-class BidangModel(IntEnum):
-    Science = 1
-    Humanities = 2
+class BidangModel(Enum):
+    Science = "science"
+    Humanities = "humanities"
+
+class penjurusan(BaseModel):
+    Science = str
+    Humanities = str
+
 
 class ScienceModel(BaseModel):
-    bidang = BidangModel.Science
-    Biology:int
-    Chemistry: int 
+    Biology: int
+    Chemistry: int
     Physics: int
     Mathematics: int
-    Reading_Comprehension_Writing_Science : int
+    Reading_Comprehension_Writing_Science: int
     General_Reasoning_Science: int
     Quantitative_Skills_Science: int
     General_Knowledge_Understanding_Science: int
+
 
 class HumanitiesModel(BaseModel):
     bidang = BidangModel.Humanities
@@ -35,6 +49,7 @@ class HumanitiesModel(BaseModel):
     General_Reasoning_Humanities: int
     Quantitative_Skills_Humanities: int
     General_Knowledge_Understanding_Humanities: int
+
 
 class TargetModel(BaseModel):
     MajorId: int
