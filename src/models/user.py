48623bce79dd196dc92bc571db447e933cb492fd
AndrosaKrist <androsa029@gmail.com>
2022-06-09 19:54:50 +0700

    fix score data type
    Signed-off-by: AndrosaKrist <androsa029@gmail.com>

--- src/models/user.py
+++ src/models/user.py
@@ -1,23 +1,23 @@
-from dataclasses import field
-import email
+# import email
 import uuid as uuid_pkg
 
-
-from enum import Enum
-from pydantic import BaseModel, Field
-from typing import Optional, Union
+from enum import Enum, IntEnum
+from pydantic import BaseModel, Field, EmailStr
+from typing import Union
 
 
 
 class UserModel(BaseModel):
-    user_id: uuid_pkg.UUID = field(
+    user_id: uuid_pkg.UUID = Field(
         default_factory= uuid_pkg.uuid4, nullable = False)
     # email: 
     nama: Union[str, None] = Field(
-        default=None, max_length=32
-    )    
-    gelombang: list[int] = 1
+        default=None, max_length=32     
+    )
 
+class gelModel(IntEnum):
+    gelombang_satu= 1
+    gelombang_dua= 2
 
 class BidangModel(Enum):
     Science = "science"
@@ -27,30 +27,40 @@ class penjurusan(BaseModel):
     Science = str
     Humanities = str
 
-
 class ScienceModel(BaseModel):
-    Biology: int
-    Chemistry: int
-    Physics: int
-    Mathematics: int
-    Reading_Comprehension_Writing_Science: int
-    General_Reasoning_Science: int
-    Quantitative_Skills_Science: int
-    General_Knowledge_Understanding_Science: int
+    Biology: float
+    Chemistry: float
+    Physics: float
+    Mathematics: float
+    Reading_Comprehension_Writing_Science: float
+    General_Reasoning_Science: float
+    Quantitative_Skills_Science: float
+    General_Knowledge_Understanding_Science: float
 
 
 class HumanitiesModel(BaseModel):
     bidang = BidangModel.Humanities
-    Economy: int
-    History: int
-    Geography: int
-    Sociology: int
-    Reading_Comprehension_Writing_Humanities: int
-    General_Reasoning_Humanities: int
-    Quantitative_Skills_Humanities: int
-    General_Knowledge_Understanding_Humanities: int
+    Economy: float
+    History: float
+    Geography: float
+    Sociology: float
+    Reading_Comprehension_Writing_Humanities: float
+    General_Reasoning_Humanities: float
+    Quantitative_Skills_Humanities: float
+    General_Knowledge_Understanding_Humanities: float
 
 
 class TargetModel(BaseModel):
     MajorId: int
     UnivId: int
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+    
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Union[str, None] = None
