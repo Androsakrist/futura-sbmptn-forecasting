34609ce5e815adebcd21663647bd78bae45856a4
AndrosaKrist <androsa029@gmail.com>
2022-06-10 21:07:16 +0700

    endpoint to get the output of the ml is 50%complete
    Signed-off-by: AndrosaKrist <androsa029@gmail.com>

--- src/endpoints/user.py
+++ src/endpoints/user.py
@@ -1,20 +1,62 @@
+from array import array
+from statistics import mean
 from fastapi import Body, Query, APIRouter
 from src.models.user import BidangModel, HumanitiesModel, ScienceModel, TargetModel, UserIn, UserModel, UserOut, gelModel
 from typing import Optional
+import pickle  
 
 # APIRouter creates path operations for user module
 router = APIRouter(
     prefix="/users",
     tags=["User"],
-    responses={404: {"description": "Not found"}},
+    responses={404: {"description": "Not found"}},  
 )
 
 @router.get("/detail")
-async def read_users(q: Optional[str] = Query(None, max_length=50)):
+def read_users(q: Optional[str] = Query(None, max_length=50)):
     results = {"users": [{"id": 1}, {"id": 2}]}
     if q:
         results.update({"q": q})
     return results
+    
+@router.post("/makeTarget/")
+async def send_score(science:ScienceModel, target:TargetModel):
+    pickle_in = open("model_science_tree.pkl","rb")
+    regressor=pickle.load(pickle_in)
+    saintek = [science.Biology, science.Physics, science.Chemistry, science.Mathematics]
+    rata2_saintek = mean(saintek)
+    pot_saintek = [science.Reading_Comprehension_Writing_Science, 
+                        science.General_Reasoning_Science, 
+                        science.Quantitative_Skills_Science, 
+                        science.General_Knowledge_Understanding_Science]
+    mean_saintek_pot= mean(pot_saintek)
+    # Biology=science['Biology']
+    # Physics=science['Physics']
+    # Chemistry=science['Chemistry']
+    # Reading_Comprehension_Writing_Science=science['Reading Comprehension Writing Science']
+    # Biology=science['Biology']
+    # Biology=science['Biology']
+    # Biology=science['Biology']
+    # prediction = regressor.predict([[Biology, Physics, Chemistry,]])
+    maketarget = regressor.predict([[
+        target.MajorId, 
+        science.Biology,
+        science.Physics,
+        science.Chemistry,
+        science.Reading_Comprehension_Writing_Science,
+        science.General_Reasoning_Science,
+        science.Quantitative_Skills_Science,
+        science.Mathematics,
+        science.General_Knowledge_Understanding_Science,
+        rata2_saintek,
+        mean_saintek_pot,
+        target.capacity,
+        ]])
+    output= round(maketarget[0],2)
+    return{
+        'hasil prediksi target kamu adalah: {}'.format(output)
+    }
+
 
 
 # @router.get("/")
@@ -29,25 +71,6 @@ async def create_user(user: UserIn):
 @router.post("/addUser")
 async def add_user(user: UserModel, bidang: BidangModel, gelombang: gelModel):
     return {"id": user.user_id, "name": user.nama, "bidang": bidang, "gelombang": gelombang.value}
-
-@router.post("/makeTarget")
-async def make_target(target: TargetModel):
-    return{"university": target.UnivId, "major": target.MajorId}
-
-
-@router.post("/sendScore/Science")
-async def send_score(science: ScienceModel):
-    return{
-        "Biology": science.Biology,
-        "Chemistry": science.Chemistry,
-        "Physics": science.Physics,
-        "Mathematics": science.Mathematics,
-        "Reading_Comprehension_Writing_Science": science.Reading_Comprehension_Writing_Science,
-        "General_Reasoning_Science": science.General_Reasoning_Science,
-        "Quantitative_Skills_Science": science.Quantitative_Skills_Science,
-        "General_Knowledge_Understanding_Science": science.General_Knowledge_Understanding_Science
-    }
-
 
 @router.post("/sendScore/Humanities")
 async def send_score(bidang: BidangModel, humanities: HumanitiesModel):
