1ba288b81d9364f07078779f93a1604f14524772
AndrosaKrist <androsa029@gmail.com>
2022-06-10 22:54:32 +0700

    fetch humanities model output
    Signed-off-by: AndrosaKrist <androsa029@gmail.com>

--- src/endpoints/user.py
+++ src/endpoints/user.py
@@ -12,15 +12,22 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},  
 )
 
-@router.get("/detail")
-def read_users(q: Optional[str] = Query(None, max_length=50)):
-    results = {"users": [{"id": 1}, {"id": 2}]}
-    if q:
-        results.update({"q": q})
-    return results
-    
-@router.post("/makeTarget/")
-async def send_score(science:ScienceModel, target:TargetModel):
+# @router.get("/detail")
+# def read_users(q: Optional[str] = Query(None, max_length=50)):
+#     results = {"users": [{"id": 1}, {"id": 2}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @router.get("/detail")
+# def read_users(q: Optional[str] = Query(None, max_length=50)):
+#     results = {"users": [{"id": 1}, {"id": 2}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+@router.post("/makeTarget/science")
+async def Science(target:TargetModel, science:ScienceModel):
     pickle_in = open("model_science_tree.pkl","rb")
     regressor=pickle.load(pickle_in)
     saintek = [science.Biology, science.Physics, science.Chemistry, science.Mathematics]
@@ -30,14 +37,6 @@ async def send_score(science:ScienceModel, target:TargetModel):
                         science.Quantitative_Skills_Science, 
                         science.General_Knowledge_Understanding_Science]
     mean_saintek_pot= mean(pot_saintek)
-    # Biology=science['Biology']
-    # Physics=science['Physics']
-    # Chemistry=science['Chemistry']
-    # Reading_Comprehension_Writing_Science=science['Reading Comprehension Writing Science']
-    # Biology=science['Biology']
-    # Biology=science['Biology']
-    # Biology=science['Biology']
-    # prediction = regressor.predict([[Biology, Physics, Chemistry,]])
     maketarget = regressor.predict([[
         target.MajorId, 
         science.Biology,
@@ -54,43 +53,36 @@ async def send_score(science:ScienceModel, target:TargetModel):
         ]])
     output= round(maketarget[0],2)
     return{
-        'hasil prediksi target kamu adalah: {}'.format(output)
+        'hasil prediksi target kamu adalah: {}%'.format(output)
     }
 
-
-
-# @router.get("/")
-# async def read_user():
-#     return{"id": .user_id, "full_name": userM.nama, "email": "danny.manny@gmail.com"}
-
-@router.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
-
-
-@router.post("/addUser")
-async def add_user(user: UserModel, bidang: BidangModel, gelombang: gelModel):
-    return {"id": user.user_id, "name": user.nama, "bidang": bidang, "gelombang": gelombang.value}
-
-@router.post("/sendScore/Humanities")
-async def send_score(bidang: BidangModel, humanities: HumanitiesModel):
+@router.post("/makeTarget/humanities")
+async def Humanities(target:TargetModel, humanities:HumanitiesModel):
+    pickle_in = open("model_humanities_tree.pkl","rb")
+    regressor=pickle.load(pickle_in)
+    soshum = [humanities.Economy, humanities.History, humanities.Geography, humanities.Sociology, humanities.Mathematics]
+    rata2_soshum = mean(soshum)
+    pot_soshum = [humanities.Reading_Comprehension_Writing_Humanities, 
+                        humanities.General_Reasoning_Humanities, 
+                        humanities.Quantitative_Skills_Humanities, 
+                        humanities.General_Knowledge_Understanding_Humanities]
+    mean_soshum_pot= mean(pot_soshum)
+    maketarget = regressor.predict([[
+        target.MajorId, 
+        humanities.Economy,
+        humanities.Geography,
+        humanities.History,
+        humanities.Sociology,
+        humanities.Reading_Comprehension_Writing_Humanities,
+        humanities.General_Reasoning_Humanities,
+        humanities.Quantitative_Skills_Humanities,
+        humanities.Mathematics,
+        humanities.General_Knowledge_Understanding_Humanities,
+        rata2_soshum,
+        mean_soshum_pot,
+        target.capacity,
+        ]])
+    output= round(maketarget[0],2)
     return{
-        "bidang": bidang,
-        "Economy": humanities.Economy,
-        "History": humanities.History,
-        "Geography": humanities.Geography,
-        "Sociology": humanities.Sociology,
-        "Reading_Comprehension_Writing_Humanities": humanities.Reading_Comprehension_Writing_Humanities,
-        "General_Reasoning_Humanities": humanities.General_Reasoning_Humanities,
-        "Quantitative_Skills_Humanities": humanities.Quantitative_Skills_Humanities,
-        "General_Knowledge_Understanding_Humanities": humanities.General_Knowledge_Understanding_Humanities
+        'hasil prediksi target kamu adalah: {}%'.format(output)
     }
-
-
-@router.put("/updateUser")
-async def add_user(user: UserModel, bidang: BidangModel, gelombang: gelModel = Body(..., description="choose gelombang")):
-    return {"id": user.user_id, "name": user.nama, "bidang": bidang, "gelombang": gelombang.value}
-
-@router.delete("/{user_id}/delete")
-async def read_user(user_id: int):
-    return {"id": user_id, "is_deleted": True}
